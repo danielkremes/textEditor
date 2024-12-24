f0236cccb1409335cd86a8a1e921f0a196cbe4d1
DanielKremes <danielkremes06@gmail.com>
2024-12-24 13:52:31 -0300

    finished functions Menu(),openFile(),editFile()

--- textEditor/Program.cs
+++ textEditor/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("What you wished");
+        Console.WriteLine("Open file (1)");
+        Console.WriteLine("Create file (2)");
+        Console.WriteLine("Quit Program (0)");
+        short option = short.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 0:
+                System.Environment.Exit(0);
+                break;
+            case 1:
+                openFile();
+                break;
+            case 2:
+                editFile();
+                break;
+            default:
+                Menu();
+                break;
+        }
+
+        static void openFile() { 
+            Console.Clear();
+        }
+        static void editFile() { 
+            Console.Clear();
+        }
+
+    }
+}
